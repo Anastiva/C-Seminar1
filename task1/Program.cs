09ae9cbf9ba64cc3fe2fa383aa4377348b95e4e2
Anastasia Chizhikova <anastiva96@yandex.ru>
2022-12-23 14:17:37 +0300

    DZ

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите переменную a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите переменную b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(b == a * a)
+    {
+        Console.WriteLine("Да");
+    }
+
+else
+    {
+    Console.WriteLine("Нет");
+    }
---
+++ task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n >= 100 && n < 1000)
+{
+    Console.Write("Последняя цифра: ");
+    Console.Write (n % 10);
+}
+else
+{
+    Console.Write("Ошибка");
+}
